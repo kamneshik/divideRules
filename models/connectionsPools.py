0fb1aa9c6f89ac5ca62361597bccebbd57eebca3
Maksim Korolev <mmmmm@kkkk.zzz>
2023-09-25 16:34:50 +0300

    Убран класс для создания пула соединений с Redis, будет использоваться класс ConnectionPool из redis.asyncio

--- models/connectionsPools.py
+++ models/connectionsPools.py
@@ -1,4 +1,4 @@
-import aioredis
+from redis.asyncio import ConnectionPool
 import pymongo
 
 from config import mongo_max_connections, mongo_host, mongo_port, mongo_min_connections
@@ -33,34 +33,3 @@ class MongoPoolConnection:
     def close_all_connections(self):
         for connection in self.connections:
             connection.close()
-
-
-class RedisConnectionPool:
-    def __init__(self, uri=f"redis://{redis_host}:{redis_port}",
-                 min_connections=redis_min_connections,
-                 max_connections=redis_max_connections
-                 ):
-        self.uri = uri
-        self.min_connections = min_connections
-        self.max_connections = max_connections
-        self.connections = None
-
-    async def initialize_pool(self):
-        self.connections = await aioredis.create_pool(
-            self.uri,
-            minsize=self.min_connections,
-            maxsize=self.max_connections
-        )
-
-    async def get_connection(self) -> aioredis.Connection:
-        if self.connections is None:
-            await self.initialize_pool()
-        return await self.connections.acquire()
-
-    async def return_connection(self, connection: aioredis.Connection):
-        await self.connections.realease(connection)
-
-    async def close_all_connections(self):
-        if self.connections is not None:
-            self.connections.close()
-            await self.connections.wait_closed()
